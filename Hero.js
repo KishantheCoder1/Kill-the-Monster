a999e23b412fb6343fc6ab422579ae00ca5d8f6f
KishantheCoder1 <76677972+KishantheCoder1@users.noreply.github.com>
2021-02-07 11:06:37 +0530

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,25 @@
+class Hero{
+    constructor(x, y, width, height){
+        var options = {
+            'density' : 1,
+            'frictionAir' : 0.7
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.image = loadImage("images/Superhero-01.png");
+        World.add(world, this.body);
+    
+    } display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 10, 10, this.width, this.height);
+        pop();
+
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,117 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12, box13, box14, box15, box16, box17, box18, box19, box20, box21, box22, box23, box24, box25, box26, box27, box28, box29;
+var ground;
+var Superhero, supIMG;
+var monster, monIMG;
+var rope;
+var backgroundIMG;
+
+function preload() {
+//preload the images here
+backgroundIMG = loadImage("images/GamingBackground.png");
+
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  // create sprites here
+
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(600,650,1200,20);
+
+  box1 = new Box(300,100,70,70);
+  box2 = new Box(300,100,70,70);
+  box3 = new Box(300,100,70,70);
+  box4 = new Box(300,100,70,70);
+  box5 = new Box(300,100,70,70);
+  box6 = new Box(300,100,70,70);
+  box7 = new Box(200,100,70,70);
+  box8 = new Box(200,100,70,70);
+  box9 = new Box(200,100,70,70);
+  box10 = new Box(200,100,70,70);
+  box11 = new Box(200,100,70,70);
+  box12 = new Box(200,100,70,70);
+  box13 = new Box(700,100,70,70);
+  box14 = new Box(700,100,70,70);
+  box15 = new Box(700,100,70,70);
+  box16 = new Box(700,100,70,70);
+  box17 = new Box(700,100,70,70);
+  box18 = new Box(700,100,70,70);
+  box19 = new Box(700,100,70,70);
+  box20 = new Box(700,100,70,70);
+  box21 = new Box(200, 100, 70, 70);
+  box22 = new Box(200, 100, 70, 70);
+  box23 = new Box(600, 100, 70, 70);
+  box24 = new Box(600, 100, 70, 70)
+  box25 = new Box(600, 100, 70, 70)
+  box26 = new Box(600, 100, 70, 70)
+  box27 = new Box(600, 100, 70, 70);
+  box28 = new Box(600, 100, 70, 70);
+  box29 = new Box(600, 100, 70, 70);
+
+  Superhero = new Hero(400,3, 100, 100);
+
+  rope = new Rope(Superhero.body,{x: 600, y: 350});
+
+  monster = new Monster(800, 200, 90, 90);
+
+}
+
+function draw() {
+  background(backgroundIMG);
+  Engine.update(engine);
+
+  //stroke("black");
+  //textSize(30);
+  //text("THE GRAY BALL IS THE HERO AND THE YELLOW ONE IS THE ENEMY!!!! GO SAVE THE WORLD", 100,50);
+  
+  ground.display();
+
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+  box7.display();
+  box8.display();
+  box9.display();
+  box10.display();
+  box11.display();
+  box12.display();
+  box13.display();
+  box14.display();
+  box15.display();
+  box16.display();
+  box17.display();
+  box18.display();
+  box19.display();
+  box20.display();
+  box21.display();
+  box22.display();
+  box23.display();
+  box24.display();
+  box25.display();
+  box26.display();
+  box27.display();
+  box28.display();
+  box29.display();
+
+  Superhero.display();
+
+  rope.display();
+
+  monster.display();
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(Superhero.body, {x: mouseX, y: mouseY});
+}
+
